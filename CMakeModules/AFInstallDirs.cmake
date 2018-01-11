05d65a16232677101837945d92ba736538d3bcd4
pradeep <pradeep@arrayfire.com>
2018-01-11 03:52:24 -0500

    cmake modifations to generate installers using cpack
    * Organise all prerequisites as cpack components
    
    * cpack generators supported
      - Linux: STGZ; IFW
      - OSX: productbuild; IFW
      - Windows: NSIS64; IFW
    
    * Forge component/dev-package is included in the installer packages only if
      requested explicitly while generating the installer. Note that this is not
      same as the forge dependency by arrayfire library
    
    * Adds ISSL(Intel Simplified Software License) to LICENSES folder and installers
    
    * Adds support use static glbinding if the project is cloned to `external/`
      folder under the project root
    
    * Removes build_glbinding.cmake file as it is not used anymore
    
    * Fixes ASSETS_DIR variable description & path issues
    
    * Marks some MKL variables as advanced in cmake
    
    * Adds additional PATH hints & SUFFIXES to FindMKL script
    
    * Links with static freeimage lib when requested in CMake
    
    * Fixes FindFreeImage cmake script to handle dll files
    
    IFW based installers give uniform look and functionality across all platforms.
    NSIS Installer can add AF_PATH environment variable and modify PATH variable.
    However, IFW installer can't do those yet.

--- CMakeModules/AFInstallDirs.cmake
+++ CMakeModules/AFInstallDirs.cmake
@@ -6,7 +6,7 @@
 
 # Executables
 if(NOT DEFINED AF_INSTALL_BIN_DIR)
-  set(AF_INSTALL_BIN_DIR "bin" CACHE PATH "Installation path for executables")
+  set(AF_INSTALL_BIN_DIR "lib" CACHE PATH "Installation path for executables")
 endif()
 
 # Libraries
@@ -14,33 +14,46 @@ if(NOT DEFINED AF_INSTALL_LIB_DIR)
   set(AF_INSTALL_LIB_DIR "lib" CACHE PATH "Installation path for libraries")
 endif()
 
+if(NOT DEFINED AF_INSTALL_DEP_BIN_DIR)
+  set(AF_INSTALL_DEP_BIN_DIR "${AF_INSTALL_BIN_DIR}/arrayfire" CACHE PATH "Installation path for arrayfire dependencies")
+endif()
+
 # Header files
 if(NOT DEFINED AF_INSTALL_INC_DIR)
   set(AF_INSTALL_INC_DIR "include" CACHE PATH "Installation path for headers")
 endif()
 
-# Data files
-if(NOT DEFINED AF_INSTALL_DATA_DIR)
-  set(AF_INSTALL_DATA_DIR "share/ArrayFire" CACHE PATH "Installation path for data files")
-endif()
+set(DATA_DIR "share/ArrayFire")
 
 # Documentation
 if(NOT DEFINED AF_INSTALL_DOC_DIR)
-  set(AF_INSTALL_DOC_DIR "${AF_INSTALL_DATA_DIR}/doc" CACHE PATH "Installation path for documentation")
+  if (WIN32)
+    set(AF_INSTALL_DOC_DIR "doc" CACHE PATH "Installation path for documentation")
+  else ()
+      set(AF_INSTALL_DOC_DIR "${DATA_DIR}/doc" CACHE PATH "Installation path for documentation")
+  endif ()
 endif()
 
 if(NOT DEFINED AF_INSTALL_EXAMPLE_DIR)
-  set(AF_INSTALL_EXAMPLE_DIR "${AF_INSTALL_DATA_DIR}/examples" CACHE PATH "Installation path for examples")
+  if (WIN32)
+    set(AF_INSTALL_EXAMPLE_DIR "examples" CACHE PATH "Installation path for examples")
+  else ()
+    set(AF_INSTALL_EXAMPLE_DIR "${DATA_DIR}/examples" CACHE PATH "Installation path for examples")
+  endif ()
 endif()
 
 # Man pages
 if(NOT DEFINED AF_INSTALL_MAN_DIR)
-  set(AF_INSTALL_MAN_DIR "${AF_INSTALL_DATA_DIR}/man" CACHE PATH "Installation path for man pages")
+    set(AF_INSTALL_MAN_DIR "${DATA_DIR}/man" CACHE PATH "Installation path for man pages")
 endif()
 
 # CMake files
 if(NOT DEFINED AF_INSTALL_CMAKE_DIR)
-  set(AF_INSTALL_CMAKE_DIR "${AF_INSTALL_DATA_DIR}/cmake" CACHE PATH "Installation path for CMake files")
+  if (WIN32)
+    set(AF_INSTALL_CMAKE_DIR "cmake" CACHE PATH "Installation path for CMake files")
+  else ()
+    set(AF_INSTALL_CMAKE_DIR "${DATA_DIR}/cmake" CACHE PATH "Installation path for CMake files")
+  endif ()
 endif()
 
 mark_as_advanced(
