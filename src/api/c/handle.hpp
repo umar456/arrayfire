99e8c27110732450f3ed66306e694cda8f882846
Umar Arshad <umar@arrayfire.com>
2022-07-08 19:19:49 -0400

    Add function to create an af_array from a device pointer

--- src/api/c/handle.hpp
+++ src/api/c/handle.hpp
@@ -9,6 +9,7 @@
 
 #pragma once
 #include <Array.hpp>
+#include <Policy.hpp>
 #include <backend.hpp>
 #include <common/err_common.hpp>
 #include <common/traits.hpp>
@@ -30,6 +31,14 @@ af::dim4 verifyDims(const unsigned ndims, const dim_t *const dims);
 af_array createHandle(const af::dim4 &d, af_dtype dtype);
 
 af_array createHandleFromValue(const af::dim4 &d, double val, af_dtype dtype);
+
+/// This function creates an af_array handle from memory handle on the device.
+///
+/// \param[in] d The shape of the new af_array
+/// \param[in] dtype The type of the new af_array
+/// \param[in] data The handle to the device memory
+/// \returns a new af_array with a view to the \p data pointer
+af_array createHandleFromDeviceData(const af::dim4 &d, af_dtype dtype, detail::Policy::MemoryType data);
 
 template<typename To>
 detail::Array<To> castArray(const af_array &in);
